2a66fae3d0c13c522f7ca5ebf6ba1c9d10a99a77
jimena-perez <jimenaperez2511@gmail.com>
2026-05-12 08:28:29 -0600

    Agregadas capas de logica y control

---
+++ Cliente.java
@@ -0,0 +1,31 @@
+public class Cliente {
+    private String id;
+    private String nombre;
+    private String email;
+
+    public Cliente(String id, String nombre, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.email = email;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
